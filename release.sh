07902148685dd38ca954077616ec6a141ab34224
Tyler Freeman <fuego@google.com>
2020-12-01 17:58:02 -0800

    Add release.sh to generate dist binaries

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Generating release binaries..."
+
+pushd web-server/tweakr-server
+npm run github
+popd
+
+pushd android-sample/
+
+./gradlew :tweakr:assembleRelease
+cp ../android/tweakr/build/outputs/aar/tweakr-release.aar ../dist/
+
+./gradlew :tweakr-firebase:assembleRelease
+cp ../android/tweakr-firebase/build/outputs/aar/tweakr-firebase-release.aar ../dist/
+
+popd
